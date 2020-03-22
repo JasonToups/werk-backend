9171826443a6579e80db86ea5e679a8823a105eb
jason-toups <me@jasontoups.com>
2020-03-22 12:51:08 -0700

    updates 404 message

--- middleware/utils.js
+++ middleware/utils.js
@@ -10,7 +10,7 @@ const methodNotAllowed = (request, response) => {
 const notFound = (request, response) => {
   response
     .status(404)
-    .send('<h1>404</h1><h3>You used splash! It was not effective.</h3>');
+    .send('<h1>404</h1><h3>You got no queens! How sad.</h3>');
 };
 
 module.exports = {
